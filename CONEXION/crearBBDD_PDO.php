b662bb2512f33e0779560a6c26c296a1fd8a3489
dapinvax <dapinva95@educarex.es>
2021-11-23 08:50:01 +0100

    Añadido fichero de configuración y ficheros de conexionPDO

---
+++ CONEXION/crearBBDD_PDO.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+define("SERVIDOR", "localhost");
+
+define("USUARIO", "root");
+
+define("CLAVE", "");
+
+
+
+try {
+
+ $con = new PDO("mysql:host=".SERVIDOR, USUARIO, CLAVE);
+
+ // Establecemos el modo de error de PDO para que salten excepciones
+
+ $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ $sql = "CREATE DATABASE miBBDD2";
+
+ // Se usa exec() porque no se devuelven resultados
+
+ $con->exec($sql);
+
+ echo "<p>Base de datos creada con éxito<br>";
+
+} catch(PDOException $e) {
+
+ echo $sql . "<br>" . $e->getMessage();
+
+}
+
+
+
+//Cerramos la conexión
+
+$con = null;
+
+
+
+?>
