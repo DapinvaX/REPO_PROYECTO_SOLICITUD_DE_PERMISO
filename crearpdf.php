d4d3f85166c85a78d3d97e84cee07c1e3bb9592b
Adrian-DWES <agolteanu01@educarex.es>
2021-11-30 10:15:19 +0100

    Bárbaras mejoras en el formato y legibilidad

--- crearpdf.php
+++ crearpdf.php
@@ -5,28 +5,23 @@
 
 use Spipu\Html2Pdf\Html2Pdf;
 
-
 //Recoger el contenido del fichero del anexo que contiene el html
-try{
+try {
+    ob_start();
 
 
-ob_start();
+    require 'permisoPDF.html';
 
 
-require 'permisoPDF.html';
+    $html=ob_get_clean();
 
 
-$html=ob_get_clean();
+    $html2pdf=new Html2Pdf('P', 'A4', 'es', 'true', 'UTF-8');
 
+    $html2pdf->writeHTML($html);
 
-$html2pdf=new Html2Pdf('P','A4','es','true','UTF-8');
-
-$html2pdf->writeHTML($html);
-
-$html2pdf->output();
-
-}catch (Exception $ex){
-    
+    $html2pdf->output();
+} catch (Exception $ex) {
     echo $ex->getMessage();
 }
 ?>
--- permiso.php
+++ permiso.php
@@ -2,80 +2,63 @@
 
 
 
-require ("./CONEXION/conexionPDO.php");
+require("./CONEXION/conexionPDO.php");
 
 
 
-class permiso{
-
-
-
+class permiso
+{
     private $conexion;
 
    
 
 
-   /*function paciente()
+    /*function paciente()
 
-   {
+    {
 
-      $this->conexion = obtenerConexionBD();
-
-     
-
-   }*/
-
-   public function __construct()
-{
-   $this->conexion = obtenerConexionBD();
-}
+       $this->conexion = obtenerConexionBD();
 
 
 
-function listar_permisos(){
+    }*/
 
-  
-   try {
-   
-      $stmt = $this->conexion->prepare("SELECT id, nombre, dni, telefono, bloque, fechaIni, fechaFin FROM permisos");
+    public function __construct()
+    {
+        $this->conexion = obtenerConexionBD();
+    }
+
+
+
+    public function listar_permisos()
+    {
+        try {
+            $stmt = $this->conexion->prepare("SELECT id, nombre, dni, telefono, bloque, fechaIni, fechaFin FROM permisos");
 
       
 
-       $stmt->execute();
-      
-       
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        return $stmt;
+    }
 
 
-   } catch(PDOException $e) {
+    public function alta_Permiso($nombre, $dni, $telefono, $bloque, $fechaIni, $fechaFin)
+    {
+        try {
+            $nombre_Permiso = "";
+            $dni_Permiso = "";
+            $telefono_Permiso = "";
+            $bloque_Permiso = "";
+            $fechaIni_Permiso = "";
+            $fechaFin_Permiso = "";
 
-   echo "Error: " . $e->getMessage();
+            // preparar y vincular parámetros
 
-   }
-
-   return $stmt;
-
-
-
-}
-
-
-   function alta_Permiso($nombre, $dni, $telefono, $bloque, $fechaIni, $fechaFin){
-
-
-
-       try {
-
-         
-         $nombre_Permiso = "";
-         $dni_Permiso = "";
-         $telefono_Permiso = "";
-         $bloque_Permiso = "";
-         $fechaIni_Permiso = "";
-         $fechaFin_Permiso = "";
-
-           // preparar y vincular parámetros
-
-           $stmt = $this->conexion->prepare("INSERT INTO permisos (nombre, dni, telefono, bloque, fechaIni, fechaFin ) 
+            $stmt = $this->conexion->prepare("INSERT INTO permisos (nombre, dni, telefono, bloque, fechaIni, fechaFin ) 
                                              VALUES (:nombre, :dni, :telefono, :bloque, :fechaIni, :fechaFin)");
 
 
@@ -93,158 +76,108 @@ function listar_permisos(){
 
        
 
-           // establecemos los parámetros y ejecutamos para insertar
+            // establecemos los parámetros y ejecutamos para insertar
 
-           $nombre_Permiso = $nombre;
-           $dni_Permiso = $dni;
-           $telefono_Permiso = $telefono;
-           $bloque_Permiso = $bloque;
-           $fechaIni_Permiso = $fechaIni;
-           $fechaFin_Permiso = $fechaFin;
+            $nombre_Permiso = $nombre;
+            $dni_Permiso = $dni;
+            $telefono_Permiso = $telefono;
+            $bloque_Permiso = $bloque;
+            $fechaIni_Permiso = $fechaIni;
+            $fechaFin_Permiso = $fechaFin;
 
 
 
-           $stmt->execute();
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
 
-   
 
-    } catch(PDOException $e) {
+    public function obtener_Permiso_por_id($id)
+    {
+        try {
+            $stmt = $this->conexion->prepare("SELECT id, nombre, dni, telefono, bloque, fechaIni, fechaFin FROM permisos WHERE id=".$id);
 
-           echo "Error: " . $e->getMessage();
+      
 
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        return $stmt;
     }
 
 
 
-   }
+    public function modificar_Permiso($id, $nombre, $dni, $telefono, $bloque, $fechaIni, $fechaFin)
+    {
+        try {
+            $nombre_Permiso = "";
+            $dni_Permiso = "";
+            $telefono_Permiso = "";
+            $bloque_Permiso = "";
+            $fechaIni_Permiso = "";
+            $fechaFin_Permiso = "";
 
+            // preparar y vincular parámetros
 
-function obtener_Permiso_por_id($id){
-
-  
-   try {
-   
-      $stmt = $this->conexion->prepare("SELECT id, nombre, dni, telefono, bloque, fechaIni, fechaFin FROM permisos WHERE id=".$id);
-
-      
-
-       $stmt->execute();
-      
-       
-
-
-   } catch(PDOException $e) {
-
-   echo "Error: " . $e->getMessage();
-
-   }
-
-   return $stmt;
-
-
-
-}
-
-
-
-function modificar_Permiso($id, $nombre, $dni, $telefono, $bloque, $fechaIni, $fechaFin){
-
-   
-   try {
-
-    $nombre_Permiso = "";
-    $dni_Permiso = "";
-    $telefono_Permiso = "";
-    $bloque_Permiso = "";
-    $fechaIni_Permiso = "";
-    $fechaFin_Permiso = "";
-
-        // preparar y vincular parámetros
-
-        $stmt = $this->conexion->prepare("UPDATE permisos 
+            $stmt = $this->conexion->prepare("UPDATE permisos 
         SET nombre=:nombre, dni=:dni, telefono=:telefono, bloque=:bloque, fechaIni=:fechaIni, fechaFin=:fechaFin 
         WHERE id=:id");
 
-         $stmt->bindParam(':id', $id_Permiso);
-         $stmt->bindParam(':nombre', $nombre_Permiso);
-         $stmt->bindParam(':dni', $dni_Permiso);
-         $stmt->bindParam(':telefono', $telefono_Permiso);
-         $stmt->bindParam(':bloque', $bloque_Permiso);  
-         $stmt->bindParam(':fechaIni', $fechaIni_Permiso);
-         $stmt->bindParam(':fechaFin', $fechaFin_Permiso);    
+            $stmt->bindParam(':id', $id_Permiso);
+            $stmt->bindParam(':nombre', $nombre_Permiso);
+            $stmt->bindParam(':dni', $dni_Permiso);
+            $stmt->bindParam(':telefono', $telefono_Permiso);
+            $stmt->bindParam(':bloque', $bloque_Permiso);
+            $stmt->bindParam(':fechaIni', $fechaIni_Permiso);
+            $stmt->bindParam(':fechaFin', $fechaFin_Permiso);
 
-        // establecemos los parámetros y ejecutamos para insertar
-        $id_Permiso = $id;
-        $nombre_Permiso = $nombre;
-        $dni_Permiso = $dni;
-        $telefono_Permiso = $telefono;
-        $bloque_Permiso = $bloque;
-        $fechaIni_Permiso = $fechaIni;
-        $fechaFin_Permiso = $fechaFin;
-
-
-
-        $stmt->execute();
+            // establecemos los parámetros y ejecutamos para insertar
+            $id_Permiso = $id;
+            $nombre_Permiso = $nombre;
+            $dni_Permiso = $dni;
+            $telefono_Permiso = $telefono;
+            $bloque_Permiso = $bloque;
+            $fechaIni_Permiso = $fechaIni;
+            $fechaFin_Permiso = $fechaFin;
 
 
 
- } catch(PDOException $e) {
-
-        echo "Error: " . $e->getMessage();
-
- }
-
-   
-  
-
-}
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
 
 
-function borrar_Permiso($id){
-   //Hecho por DapinvaX
+    public function borrar_Permiso($id)
+    {
+        //Hecho por DapinvaX
 
-   try {
+        try {
+            $id_Permiso="";
 
-      $id_Permiso="";
+            // preparar y vincular parámetros
 
-       // preparar y vincular parámetros
+            $stmt = $this->conexion->prepare("DELETE FROM permisos WHERE id=:id");
 
-       $stmt = $this->conexion->prepare("DELETE FROM permisos WHERE id=:id");
-
-       $stmt->bindParam(':id', $id_Permiso);
+            $stmt->bindParam(':id', $id_Permiso);
 
       
 
-       // establecemos los parámetros y ejecutamos para insertar
+            // establecemos los parámetros y ejecutamos para insertar
 
-       $id_Permiso = $id;
-
-  
-
-       $stmt->execute();
-
-} catch(PDOException $e) {
-
-       echo "Error: " . $e->getMessage();
-
-}
-
-
-
-
+            $id_Permiso = $id;
 
   
 
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
 }
-
-
-
-
-
-}
-
-
-
-
-
 ?>
