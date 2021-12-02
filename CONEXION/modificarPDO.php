cd468e1d6da91cdaf311f145f8f83265c2092947
Adrian-DWES <agolteanu01@educarex.es>
2021-12-02 14:26:35 +0100

    xd

--- CONEXION/modificarPDO.php
+++ CONEXION/modificarPDO.php
@@ -5,7 +5,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "delete from profesor where nombre = 'john';";
+  $sql = "update profesor set nombre = 'john';";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully";
