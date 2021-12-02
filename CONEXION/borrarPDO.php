2150590f594daff02c53f5bdb91500c08315dd81
Adrian-DWES <agolteanu01@educarex.es>
2021-12-02 14:23:59 +0100

    xd

--- CONEXION/borrarPDO.php
+++ CONEXION/borrarPDO.php
@@ -5,8 +5,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO profesor (dni, nombre, telefono, firma)
-  VALUES ('X7886653E', 'paco', '612291628', 'paco')";
+  $sql = "delete from profesor where nombre = 'paco';";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully";
--- CONEXION/listarPDO.php
+++ CONEXION/listarPDO.php
@@ -5,11 +5,17 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO profesor (dni, nombre, telefono, firma)
-  VALUES ('X7886653E', 'paco', '612291628', 'paco')";
+  $sql = "select * from profesor";
   // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "New record created successfully";
+  $datos =$conn->prepare($sql);
+  $datos->execute();
+  foreach($datos as $fila){
+    echo $fila['dni'];
+    echo $fila['nombre'];
+    echo $fila['telefono'];
+    echo $fila['firma'];
+  }
+  
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
