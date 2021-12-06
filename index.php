20c74bac1401d8661d7764c235a8bc3b0f60f2e3
DapinvaX <dapinva95@gmail.com>
2021-12-06 23:01:52 +0100

    Subida de modificación de profesores.php e index.php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-//Dump de errores en el navegador
-ini_set('display_errors', 1);
-
-ini_set('display_startup_errors', 1);
-
-error_reporting(E_ALL);
-
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -41,32 +31,31 @@ error_reporting(E_ALL);
    
 <?php
 
-require_once("./CONEXION/conexionPDO.php");
-require_once("permiso.php");
+require_once("profesores.php");
 
 
-$permiso = new permiso();
+$profesor = new profesor();
 
 
 
 if (isset($_REQUEST["operacion"])) {
     if ($_REQUEST["operacion"]=="modificar") {
-        $permiso->modificar_permiso($_POST["nume"], $_POST["nombre"], $_POST["dni"], $_POST["telefono"], $_POST["bloque"], $_POST["fechaIni"], $_POST["fechaFin"]);
-        mostrarListado($permiso->listar_permisos(), -1);
+        $profesor->modificar_profesor($_POST["nume"], $_POST["nombre"], $_POST["dni"], $_POST["telefono"], $_POST["firma"]);
+        mostrarListado($profesor->listar_profesores(), -1);
     } elseif ($_REQUEST["operacion"]=="borrar") {
-        $permiso->borrar_permiso($_REQUEST["nume"]);
+        $profesor->borrar_profesor($_REQUEST["nume"]);
 
-        echo "<CENTER>Se ha borrado correctamente el permiso.</CENTER><P>";
+        echo "<CENTER>El profesor ha sido eliminado satisfactoriamente.</CENTER><P>";
 
-        mostrarListado($permiso->listar_permisos(), -1);
+        mostrarListado($profesor->listar_profesores(), -1);
     }
 } else { // no hay operacion. Ejemplo: La primera vez que se entra
-    mostrarListado($permiso->listar_permisos(), -1);
+    mostrarListado($profesor->listar_profesores(), -1);
 }
 
 
 
-function mostrarListado($permisos_array, $elemento)
+function mostrarListado($profesores_array, $elemento)
 {
     $html='
 <div class="container">
@@ -81,7 +70,7 @@ function mostrarListado($permisos_array, $elemento)
                         <th>ID</th>
                         <th>Nombre</th>
                         <th>DNI</th>
-                        <th>Fecha</th>
+                        <th>TELEFONO</th>
 
                     </tr>
                 </thead>
@@ -93,38 +82,44 @@ function mostrarListado($permisos_array, $elemento)
 
           $fila="";
 
-          print_r($permisos_array);
-          foreach ($permisos_array as $permiso) {
-              if ($elemento != $permiso['id']) {
-                  $fila='<tr id='.$permiso['id'].'>
+          //print_r($profesores_array);
+          foreach ($profesores_array as $profesor) {
+              if ($elemento != $profesor['id']) {
+                  $fila='<tr id='.$profesor['id'].'>
 
-                <td>'.$permiso['id'].'</td>
+                <td>'.$profesor['id'].'</td>
 
-                <td>'.$permiso['nombre'].'</td>
+                <td>'.$profesor['nombre'].'</td>
 
-                <td>'.$permiso['dni'].'</td>
+                <td>'.$profesor['dni'].'</td>
 
-                <td>'.$permiso['fechaIni'].'</td>
+                <td>'.$profesor['telefono'].'</td>
 
-                <td>'.'<a href="listarPermisos.php?idPermiso='.$permiso['id'].'" class="btn btn-info" role="button">Ver</a>'.'</td>
+                <td>'.$profesor['firma'].'</td>
 
-                <td>'.'<a href="index.php?operacion=editar&nume='.$permiso['id'].'" class="btn btn-warning" role="button">Modificar</a>'.'</td>
+                <td>'.'<a href="listarProfesores.php?idProfesor='.$profesor['id'].'" class="waves-effect waves-light btn  teal lighten-3" role="button">Ver</a>'.'</td>
 
-                <td>'.'<a href="index.php?operacion=borrar&nume='.$permiso['id'].'" class="btn btn-danger" role="button">Eliminar</a>'.'</td>
+                <td>'.'<a href="index.php?operacion=editar&nume='.$profesor['id'].'" class="waves-effect waves-light btn amber lighten-1" role="button">Modificar</a>'.'</td>
+
+                <td>'.'<a href="index.php?operacion=borrar&nume='.$profesor['id'].'" class="waves-effect waves-light btn red accent-4" role="button">Eliminar</a>'.'</td>
 
               </tr>';
               } else {
                   $fila='<tr><form method="POST" class="form-inline" action="index.php?operacion=modificar">
 
-                <td>'.$permiso['id'].'</td>
+                <td>'.$profesor['id'].'</td>
 
-                <td><input type="text" class="form-control" id="nombre" value="'.$permiso['nombre'].'" name="nombre" style="width:150px"></td>
+                <td><input type="text" class="form-control" id="nombre" value="'.$profesor['nombre'].'" name="nombre" style="width:150px"></td>
 
-                <td><input type="text" class="form-control" id="dni" value="'.$permiso['dni'].'" name="dni" style="width:150px"></td>
+                <td><input type="text" class="form-control" id="dni" value="'.$profesor['dni'].'" name="dni" style="width:150px"></td>
 
-                <td>'.'<input type="submit" class="btn btn-success"  value="Guardar" />'.'</td>
+                <td><input type="text" class="form-control" id="telefono" value="'.$profesor['telefono'].'" name="telefono" style="width:150px"></td>
 
-                <td>'.'<a href="index.php" class="btn btn-danger" role="button">Cancelar</a>'.'</td>
+                <td><input name="firma" id="firma" type="file"/></td>
+
+                <td>'.'<input type="submit" class="waves-effect waves-light btn green accent-3"  value="Guardar" name="guardar" />'.'</td>
+
+                <td>'.'<a href="index.php" class="waves-effect waves-light btn red accent-4" role="button">Cancelar</a>'.'</td>
 
                 <input type="hidden" name="nume" value="'.$elemento.'" />
 
@@ -136,15 +131,12 @@ function mostrarListado($permisos_array, $elemento)
             
 
               $html=$html.$fila;
-          } //fin del bucle for
+          } //fin del bucle
 
           $html=$html.'</tbody>
 
                           </table>
 
-                          <div id="informacion">
-                          
-                          </div>
 
                       </div>';
 
@@ -152,11 +144,66 @@ function mostrarListado($permisos_array, $elemento)
 }
 
 
-
+//Código para subir imagen de firma
+if (isset($_POST['guardar'])) {
+  //Recogemos el archivo enviado por el formulario
+  $firma = $_FILES['firma']['name'];
+  //Si el archivo contiene algo y es diferente de vacio
+  if (isset($firma) && $firma != "") {
+     //Obtenemos algunos datos necesarios sobre el archivo
+     $tipo = $_FILES['firma']['type'];
+     $tamano = $_FILES['firma']['size'];
+     $temp = $_FILES['firma']['tmp_name'];
+     //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
+    if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 2000000))) {
+       echo '<div><b>Error. La extensión o el tamaño de los archivos no es correcta.<br/>
+       - Se permiten archivos .gif, .jpg, .png. y de 200 kb como máximo.</b></div>';
+    }
+    else {
+       //Si la imagen es correcta en tamaño y tipo
+       //Se intenta subir al servidor
+       if (move_uploaded_file($temp, 'images/'.$firma)) {
+           //Cambiamos los permisos del archivo a 777 para poder modificarlo posteriormente
+           chmod('images/'.$firma, 0777);
+           //Mostramos el mensaje de que se ha subido co éxito
+           echo '<div><b>Se ha subido correctamente la imagen.</b></div>';
+           //Mostramos la imagen subida
+           echo '<p><img src="images/'.$firma.'"></p>';
+       }
+       else {
+          //Si no se ha podido subir la imagen, mostramos un mensaje de error
+          echo '<div><b>Ocurrió algún error al subir el fichero. No pudo guardarse.</b></div>';
+       }
+     }
+  }
+}
 
 
 ?>
 
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <input type="button" class="waves-effect waves-light btn  teal lighten-3" id="myBtn" value="Crear nuevo formulario">
+    </div>
+
+    <div class="col-md-6">
+      <input type="button" class="waves-effect waves-light btn  teal lighten-3" id="myBtn2" value="Crear nuevo PDF">
+    </div>
+  </div>
+</div>
+<script>
+
+            var btn = document.getElementById('myBtn');
+            btn.addEventListener('click', function () {
+                document.location.href = 'formularioPermiso.php';
+            });
+
+            var btn = document.getElementById('myBtn2');
+            btn.addEventListener('click', function () {
+                document.location.href = 'crearpdf.php';
+            });
+        </script>
 </body>
 
 </html>
