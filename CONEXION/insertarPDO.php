a31cfb48f7a07277f9c70f378c7489cb3af106e4
Adrian-DWES <90677174+Adrian-DWES@users.noreply.github.com>
2021-12-08 21:40:16 +0100

    Update insertarPDO.php

--- CONEXION/insertarPDO.php
+++ CONEXION/insertarPDO.php
@@ -1,91 +1,44 @@
 <?php
 //Dump de errores en el navegador
 ini_set('display_errors', 1);
-
 ini_set('display_startup_errors', 1);
-
 error_reporting(E_ALL);
-
 ?>
 
 <?php
 
 //Método 1: Añadiendo el archivo conexion.php que obtiene la conexión
-
 // leyendo los datos de conexión de un fichero .ini
-
 include_once 'conexionPDO.php';
 
 
 
 //Método 2: Usando un array para almacenar los datos de conexión.
-
 // Este array puede ir fuera o dentro del fichero que va a usar la base de datos.
-
 $dbConfig = array(
 
-   'host' => 'localhost',
-
-   'port' => '3306',
-
-   'user' => 'root',
-
-   'pass' => 'admin1234',
-
-   'db' => 'dawBD',
-
+    'host' => 'localhost',
+    'port' => '3306',
+    'user' => 'root',
+    'pass' => 'admin1234',
+    'db' => 'dawBD',
 );
 
 
 
-//Método 3: Crear constantes para definir los datos de conexión.
-
+//Crear constantes para definir los datos de conexión.
 //Estas constantes pueden ir dentro o fuera del fichero que va a usar la base de datos.
-
 define("SERVIDOR", "localhost");
-
 define("USUARIO", "usuarioDAW");
-
 define("CLAVE", "admin1234");
-
 define("BBDD", "dawBD");
 
 
 
 // Crear una conexion
-
-// En la conexión, indicamos la base de datos que queremos utilizar.
-
 try {
-
-
-
-   //Método 1
-
-    //La conexión nos la proporciona conexion.php
-
-
-
-    //Método 2
-
-    //$conexion = new PDO("mysql:host=".$dbConfig['host'].";dbname=".$dbConfig['db'], $dbConfig['user'], $dbConfig['pass']);
-
-
-
-    //Método 3
-
-    //$conexion = new PDO("mysql:host=".SERVIDOR.";dbname=".BBDD, USUARIO, CLAVE);
-
-
-
-    // Establecemos el modo de error de PDO para que salten excepciones
-
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
     $sql = "INSERT INTO participantes (nombre, numero,email) VALUES ('Luis', 23, 'luismr@ies.es')";
-
     // se usa exec() porque la sentencia no devuelve ningún valor
 
     $conexion->exec($sql);
@@ -95,4 +48,4 @@ try {
     echo $sql . "<br>" . $e->getMessage();
 }
 
- $conexion = null;
+$conexion = null;
