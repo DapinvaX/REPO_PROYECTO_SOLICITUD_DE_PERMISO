b3d2052b9771df55de1a8e3104b930cb228d5a32
DapinvaX <dapinva95@gmail.com>
2021-12-09 20:20:42 +0100

    Update permisos.sql

--- permisos.sql
+++ permisos.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 -- Base de datos: `permisos`
 --
 -- --------------------------------------------------------
-
+CREATE SCHEMA IF NOT EXISTS `permisos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `permisos` ;
 --
 -- Estructura de tabla para la tabla `departamentos`
 --
