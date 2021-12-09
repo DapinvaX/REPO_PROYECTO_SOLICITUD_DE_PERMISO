c16e9375da939351e832d8659e19509beedb74c2
dapinvax <dapinva95@educarex.es>
2021-12-09 14:17:35 +0100

    Quitado Codigo Sobrante

--- getPermisos.php
+++ getPermisos.php
@@ -1,14 +1,4 @@
 <?php
-//Dump de errores en el navegador
-ini_set('display_errors', 1);
-
-ini_set('display_startup_errors', 1);
-
-error_reporting(E_ALL);
-
-?>
-
-<?php
 
 require_once("permiso.php");
 $permiso = new permiso();
